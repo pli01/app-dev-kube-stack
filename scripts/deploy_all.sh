c9a5f4e4b469a18215e88aeab2423de926faf75c
pli01 <pli01@users.noreply.github.com>
2024-02-26 23:43:01 +0100

    add local registry

--- scripts/deploy_all.sh
+++ scripts/deploy_all.sh
@@ -2,10 +2,16 @@
 #
 # deploy cluster and applications
 #
+set -eu -o pipefail
 K3D_CONFIG="${K3D_CONFIG:-k3d/kube-multiple-agent-ingress-nginx/k3d.yml}"
+#
+# registry data on volume
+export REGISTRY_DIR="${REGISTRY_DIR:-registry-data}"
+# if need persistent registry data locally , set to
+# REGISTRY_DIR=$(pwd)/registry
 
 echo "# Create intial cluster"
-k3d cluster create --config ${K3D_CONFIG} 
+k3d cluster create --config ${K3D_CONFIG}
 
 echo "# List cluster and pods"
 k3d cluster list
@@ -15,10 +21,8 @@ kubectl get node -A
 kubectl get pod -A
 
 echo "# Deploy application in cluster"
+export HELM_DIFF_THREE_WAY_MERGE=true
 helmfile apply --wait
 
 echo "# Test whoami url"
 curl http://localhost:8080
-
-#echo "# Delete cluster and all ressources"
-#k3d cluster delete --config ${K3D_CONFIG} 
